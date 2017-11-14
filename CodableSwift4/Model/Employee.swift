b8fc1858c8c55f5126c4573f90d6ef77c98bdfe7
kanchanproseth <kanchanproseth@users.noreply.github.com>
2017-11-14 10:34:03 +0700

    init

--- CodableSwift4/Model/Employee.swift
+++ CodableSwift4/Model/Employee.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+struct Employee: Codable {
+    var name: String
+    var id: Int
+    var favoriteToy: Toy
+    
+    //for change name of field when it encode to json
+    enum CodingKeys: String, CodingKey {
+        case id = "employeeId"
+        case name
+        case gift
+    }
+}
+
+struct Toy: Codable {
+    var name: String
+}
+
+extension Employee {
+    //custom the endcode result json
+    //from :
+    //{ "employeeId": 7, "name": "John Appleseed", "favoriteToy": {"name":"Teddy Bear"}}
+    //to :
+    //{ "employeeId": 7, "name": "John Appleseed", "gift": "Teddy Bear" }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(id, forKey: .id)
+        try container.encode(favoriteToy.name, forKey: .gift)
+    }
+}
+
+extension Employee {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        id = try values.decode(Int.self, forKey: .id)
+        let gift = try values.decode(String.self, forKey: .gift)
+        favoriteToy = Toy(name: gift)
+    }
+}
--- CodableSwift4/ViewController.swift
+++ CodableSwift4/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        encodeDecodeSample()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func encodeDecodeSample(){
+        let toy1 = Toy(name: "Teddy Bear");
+        let employee1 = Employee(name: "John Appleseed", id: 7, favoriteToy: toy1)
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(employee1)
+        print(jsonData!)
+        
+        let jsonString = String(data: jsonData!, encoding: .utf8)
+        print(jsonString!)
+        //decode
+        let jsonDecoder = JSONDecoder()
+        let employee2 = try? jsonDecoder.decode(Employee.self, from: jsonData!)
+        
+        print(employee2!)
+        
+        
+    }
 
 }
 
